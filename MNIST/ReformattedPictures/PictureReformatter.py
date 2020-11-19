39416065c0adc9fd05e0d91e38773140797e7bac
Marten Scheuck <martenscheuck@gmail.com>
2020-11-19 17:53:49 +0100

    Added pictures

--- MNIST/ReformattedPictures/PictureReformatter.py
+++ MNIST/ReformattedPictures/PictureReformatter.py
@@ -6,17 +6,20 @@ reformatted_path = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_filename(filename, path):
+    """Gets the filename of picture to be reformatted"""
     return os.path.join(path, filename)
 
 
 def resize(filename, path):
+    """Resizes the picture so it can be progressed by ML"""
     file = get_filename(filename, path)
     im = Image.open(file)
     imResize = im.resize((28,28), Image.ANTIALIAS)
-    imResize.save(file , 'JPEG', quality=90)
+    imResize.save(file[:-4] + "28Pixel.png", 'PNG', quality=90)
 
 
 def black_n_white(filename, path):
+    """Removes RGB and turns the picture BW"""
     file = get_filename(filename, path)
     im = Image.open(file).convert("1")
     im.save(filename[:-4] + "_BNW.png")
@@ -32,4 +35,5 @@ def setting_pixel_values(filename, path):
 
 
 if __name__ == "__main__":
-    setting_pixel_values("hand-drawn-number-1-vector-1468591_BNW.png", reformatted_path)
+    resize("Nummer6Mama.png", "Pictures")
+    resize("Nummer9Mama.png", "Pictures")
